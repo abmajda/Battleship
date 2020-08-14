c5c0b2b780834869c593f5dfc5819c355d943352
abmajda <abmajda@gmail.com>
2020-08-14 18:32:55 -0400

    Implemented the Ships

--- Battleship/Ships.cs
+++ Battleship/Ships.cs
@@ -19,22 +19,46 @@ namespace Battleship
         }
     }
 
-    abstract class Ship
+    // the base class for all ships. Abstract class all ships will derive from it
+    public abstract class Ship
     {
         protected int health;
-        protected bool horizontal;
-        List<Coords> location;
+        protected int size;
+        protected List<Coords> location;
 
-        // places the ship
+        // places the ship and initialize it's health
         public Ship(Coords start, bool horizontal)
         {
-            // constructor
+            health = size;
+            location = new List<Coords>();
+
+            if (horizontal)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    location.Add(new Coords((start.x + i), start.y));
+                }
+            }
+            
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    location.Add(new Coords(start.x , (start.y + i)));
+                }
+            }
         }
 
         // check if the coords provided is in the ships location
         public bool checkHit(Coords shot)
         {
-            // check hit function
+            foreach (Coords space in location)
+            {
+                if (shot.Equals(space))
+                    return true;
+            }
+
+            // if no match is found return false
             return false;
         }
 
@@ -46,5 +70,45 @@ namespace Battleship
             else
                 return false;
         }
+    }
+
+    // derived class for the carrier
+    public class Carrier : Ship
+    {
+        protected new int size = 5;
+        // places the ship and initialize it's health
+        public Carrier(Coords start, bool horizontal) : base(start, horizontal) { }
+    }
+
+    // derived class for the Battleship
+    public class Battleship : Ship
+    {
+        protected new int size = 4;
+        // places the ship and initialize it's health
+        public Battleship(Coords start, bool horizontal) : base(start, horizontal) { }
+    }
+
+    // derived class for the Destroyer
+    public class Destroyer : Ship
+    {
+        protected new int size = 3;
+        // places the ship and initialize it's health
+        public Destroyer(Coords start, bool horizontal) : base(start, horizontal) { }
+    }
+
+    // derived class for the Submarine
+    public class Submarine : Ship
+    {
+        protected new int size = 3;
+        // places the ship and initialize it's health
+        public Submarine(Coords start, bool horizontal) : base(start, horizontal) { }
+    }
+
+    // derived class for the PTBoat
+    public class PTBoat : Ship
+    {
+        protected new int size = 2;
+        // places the ship and initialize it's health
+        public PTBoat(Coords start, bool horizontal) : base(start, horizontal) { }
     }
 }
--- Battleship/StartScreen.Designer.cs
+++ Battleship/StartScreen.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MainLogo = new System.Windows.Forms.Label();
+            this.ShamelessPlug = new System.Windows.Forms.Label();
+            this.AIButton = new System.Windows.Forms.Button();
+            this.MPButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainLogo
+            // 
+            this.MainLogo.AutoSize = true;
+            this.MainLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainLogo.Location = new System.Drawing.Point(88, 45);
+            this.MainLogo.Name = "MainLogo";
+            this.MainLogo.Size = new System.Drawing.Size(167, 37);
+            this.MainLogo.TabIndex = 0;
+            this.MainLogo.Text = "Battleship";
+            // 
+            // ShamelessPlug
+            // 
+            this.ShamelessPlug.AutoSize = true;
+            this.ShamelessPlug.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShamelessPlug.Location = new System.Drawing.Point(74, 101);
+            this.ShamelessPlug.Name = "ShamelessPlug";
+            this.ShamelessPlug.Size = new System.Drawing.Size(198, 20);
+            this.ShamelessPlug.TabIndex = 1;
+            this.ShamelessPlug.Text = "A Project by Andrew Majda";
+            // 
+            // AIButton
+            // 
+            this.AIButton.Location = new System.Drawing.Point(12, 176);
+            this.AIButton.Name = "AIButton";
+            this.AIButton.Size = new System.Drawing.Size(100, 23);
+            this.AIButton.TabIndex = 2;
+            this.AIButton.Text = "Play against AI";
+            this.AIButton.UseVisualStyleBackColor = true;
+            // 
+            // MPButton
+            // 
+            this.MPButton.Location = new System.Drawing.Point(118, 176);
+            this.MPButton.Name = "MPButton";
+            this.MPButton.Size = new System.Drawing.Size(100, 23);
+            this.MPButton.TabIndex = 3;
+            this.MPButton.Text = "Play Multiplayer";
+            this.MPButton.UseVisualStyleBackColor = true;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(222, 176);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(100, 23);
+            this.CloseButton.TabIndex = 4;
+            this.CloseButton.Text = "Exit";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // StartScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(334, 211);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.MPButton);
+            this.Controls.Add(this.AIButton);
+            this.Controls.Add(this.ShamelessPlug);
+            this.Controls.Add(this.MainLogo);
+            this.Name = "StartScreen";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label MainLogo;
+        private System.Windows.Forms.Label ShamelessPlug;
+        private System.Windows.Forms.Button AIButton;
+        private System.Windows.Forms.Button MPButton;
+        private System.Windows.Forms.Button CloseButton;
     }
 }
 
